e337ef8d9fdf7a2b6509be78ab3df65799b7dc9d
pierresaid <said.pierre.emler@gmail.com>
2020-06-19 04:02:58 +0000

    Deploying to gh-pages from  @ 3ee552485917ecaad4f30f4eda50387d713b2563 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,16 @@
-parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"AaGI":[function(require,module,exports) {
-self.__WB_MANIFEST,importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"),workbox&&workbox.precaching.precacheAndRoute([]);
-},{}]},{},["AaGI"], null)
-//# sourceMappingURL=https://pierresaid.github.io/random-cat-facts/service-worker.js.map
+ [{"revision":"64fdbcee49e087971688604c28f3266e","url":"favicon-16x16.23b25d3d.png"},{"revision":"b0466da1d24d90b3b8a07ff6d8c89a9e","url":"favicon-32x32.5c2f78a7.png"},{"revision":"0296d7547c4f495b3d55692ec9cacc83","url":"favicon-96x96.cdb7d7dd.png"},{"revision":"9ae5e9caa70dfab31be489190e9eb292","url":"icon.6e6b9c81.png"},{"revision":"cd13c575f6fc47ab56986c72c2a117aa","url":"index.html"},{"revision":"5edfc55d50d0d6c1d05ae4d4882910bc","url":"logo.0dbcb34f.png"},{"revision":"082b43dd9e030eaa4ee3875d7a2063d4","url":"main.f7025706.css"},{"revision":"acadab010f240a3eca89dc865476f220","url":"main.f84e1103.js"},{"revision":"be2cecfca5f6231a3b19cf18d56100a8","url":"manifest.webmanifest"},{"revision":"6105216f9f69572f0f386a4eb3329720","url":"modernizr-3.11.2.min.bbf62f91.js"},{"revision":"c3f1790e5b5ebd02472343ba7746da15","url":"normalize.02b6c58e.css"},{"revision":"d121930984923110549a3fe0ed94bcbc","url":"plugins.40a68ed9.js"}]
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// Network First
+[ 
+    '/$',  // Index 
+    '/*',  // Anything in the same host
+    '.+/*' // Anything in any host 
+]
+.forEach(mask => {
+    workbox.routing.registerRoute(
+        new RegExp(mask),
+        new workbox.strategies.NetworkFirst( { cacheName: 'dynamic' } ) 
+    );
+});
