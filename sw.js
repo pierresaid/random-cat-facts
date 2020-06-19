d5a9fca267678a441aeb37ecf8a40bfd2be96967
pierresaid <said.pierre.emler@gmail.com>
2020-06-19 02:47:20 +0000

    Deploying to gh-pages from  @ 1af974a813755636546d311c142aae425ae774d1 🚀

--- sw.js
+++ sw.js
@@ -1,12 +1,7 @@
-[{"revision":"9ae5e9caa70dfab31be489190e9eb292","url":"icon.6e6b9c81.png"},{"revision":"f5455f9b2eca27ac98ac5179ded090e9","url":"index.html"},{"revision":"5edfc55d50d0d6c1d05ae4d4882910bc","url":"logo.0dbcb34f.png"},{"revision":"082b43dd9e030eaa4ee3875d7a2063d4","url":"main.f7025706.css"},{"revision":"acadab010f240a3eca89dc865476f220","url":"main.f84e1103.js"},{"revision":"6105216f9f69572f0f386a4eb3329720","url":"modernizr-3.11.2.min.bbf62f91.js"},{"revision":"c3f1790e5b5ebd02472343ba7746da15","url":"normalize.02b6c58e.css"},{"revision":"d121930984923110549a3fe0ed94bcbc","url":"plugins.40a68ed9.js"},{"revision":"b73836013f965ab2b473f1dca47c82c7","url":"service-worker.js"},{"revision":"ebf8b6d4e3624c26258f7bd96a68f210","url":"site.webmanifest"}]
+[{"revision":"64fdbcee49e087971688604c28f3266e","url":"favicon-16x16.23b25d3d.png"},{"revision":"b0466da1d24d90b3b8a07ff6d8c89a9e","url":"favicon-32x32.5c2f78a7.png"},{"revision":"0296d7547c4f495b3d55692ec9cacc83","url":"favicon-96x96.cdb7d7dd.png"},{"revision":"9ae5e9caa70dfab31be489190e9eb292","url":"icon.6e6b9c81.png"},{"revision":"b9b9a7f484d31aa3ae76ec2ff548dfae","url":"index.html"},{"revision":"5edfc55d50d0d6c1d05ae4d4882910bc","url":"logo.0dbcb34f.png"},{"revision":"082b43dd9e030eaa4ee3875d7a2063d4","url":"main.f7025706.css"},{"revision":"acadab010f240a3eca89dc865476f220","url":"main.f84e1103.js"},{"revision":"6105216f9f69572f0f386a4eb3329720","url":"modernizr-3.11.2.min.bbf62f91.js"},{"revision":"c3f1790e5b5ebd02472343ba7746da15","url":"normalize.02b6c58e.css"},{"revision":"d121930984923110549a3fe0ed94bcbc","url":"plugins.40a68ed9.js"},{"revision":"435076dc4f9692c4822fcfa547d07b83","url":"service-worker.js"},{"revision":"ebf8b6d4e3624c26258f7bd96a68f210","url":"site.webmanifest"}]
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 if (workbox) {
-    console.log(`Yay! Workbox is loaded 🎉`);
-  
     workbox.precaching.precacheAndRoute([]);
-  
-  } else {
-    console.log(`Boo! Workbox didn't load 😬`);
   }
