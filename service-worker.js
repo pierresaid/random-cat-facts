1af974a813755636546d311c142aae425ae774d1
Pierre SAID <said.pierre.emler@gmail.com>
2020-06-19 04:46:23 +0200

    Add manifest.json

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,5 @@ self.__WB_MANIFEST
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 if (workbox) {
-    console.log(`Yay! Workbox is loaded 🎉`);
-  
     workbox.precaching.precacheAndRoute([]);
-  
-  } else {
-    console.log(`Boo! Workbox didn't load 😬`);
   }
